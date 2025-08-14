0765cca44cb71e1303b61dc7ff28928158840bdb
John W. Terrell <=>
2025-08-13 21:45:41 -0700

    Added editor settings

--- config/default.nix
+++ config/default.nix
@@ -1,73 +1,94 @@
-{self, ...}:{
-	imports = [ ./bufferline.nix ];
+{ self, ... }:
+{
+  imports = [ ./bufferline.nix ];
 
-	config = {  
-		colorschemes.gruvbox.enable = true;
+  config = {
+    colorschemes.gruvbox.enable = true;
 
-		globals = {
-			mapleader = " ";
-		};
+    globals = {
+      mapleader = " ";
+    };
 
-		plugins = {
-			cmp = {
-				enable = true;
-				autoEnableSources = true;
-			};
-			lsp = {
-				enable = true;
-				servers = {
-                                        clangd = {
-						enable = true;
-						settings = {
-							cmd = [
-								"clangd"
-								"--background-index"
-							];
-							filetypes = [
-								"c"
-								"cpp"
-							];
-							root_markers = [
-								"compile_commands.json"
-								"compile_flags.txt"
-							];
-						};
-         				};
-					ts_ls.enable = true;
-					lua_ls.enable = true;
-					rust_analyzer = {
-						enable = true;
-						installCargo = true;
-						installRustc = true;
-					};
-				};
-			};
-			lualine = {
-				enable = true;
-			};
-			luasnip = {
-				enable = true;
-			};
-			oil = {
-				enable = true;
-			};
-			telescope = {
-				enable = true;
-			};
-			treesitter = {
-				enable = true;
-			};
-		};
-                vimAlias = true;
-                viAlias = true;
-	};
-
-
-	options = {
+    opts = {
+		guicursor = "";
 		number = true;
 		relativenumber = true;
 
-		shiftwidth = 2;
-	};
-}
+      	shiftwidth = 4;
+		tabstop = 4;
+		softtabstop = 4;
+		expandtab = true;
 
+		smartindent = true;
+		wrap = false;
+		swapfile = false;
+		backup = false;
+		undodir = builtins.getEnv "HOME" + ".vim/undodir";
+		undofile = false;
+
+		hlsearch = true;
+		incsearch = true;
+
+		termguicolors = true;
+		scrolloff = 98;
+		signcolumn = "yes";
+		
+		updatetime = 50;
+
+		colorcolumn = "80";
+    };
+
+    plugins = {
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+      };
+      lsp = {
+        enable = true;
+        servers = {
+          clangd = {
+            enable = true;
+            settings = {
+              cmd = [
+                "clangd"
+                "--background-index"
+              ];
+              filetypes = [
+                "c"
+                "cpp"
+              ];
+              root_markers = [
+                "compile_commands.json"
+                "compile_flags.txt"
+              ];
+            };
+          };
+          ts_ls.enable = true;
+          lua_ls.enable = true;
+          rust_analyzer = {
+            enable = true;
+            installCargo = true;
+            installRustc = true;
+          };
+        };
+      };
+      lualine = {
+        enable = true;
+      };
+      luasnip = {
+        enable = true;
+      };
+      oil = {
+        enable = true;
+      };
+      telescope = {
+        enable = true;
+      };
+      treesitter = {
+        enable = true;
+      };
+    };
+    vimAlias = true;
+    viAlias = true;
+  };
+}
