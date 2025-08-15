baef6765b03885181f402721ae6e749ddaa76673
John W. Terrell <=>
2025-08-14 20:45:49 -0700

    WIP

--- config/default.nix
+++ config/default.nix
@@ -53,6 +53,14 @@
             action = "<cmd>Telescope git_files<CR>";
             key = "<leader>pf";
         }
+        {
+            mode = "n";
+            options = {
+                expr = true;
+            };
+            action = ''function() require("telescope.builtin").grep_string( { search = vim.fn.input("Grep > ") }) end'';
+            key = "<leader>fg";
+        }
         ];
 
         plugins = {
