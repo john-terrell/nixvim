97f360be02bd7f5d17f30da3c314536ccf70e7e7
John W. Terrell <=>
2025-08-14 18:51:30 -0700

    WIP

--- config/default.nix
+++ config/default.nix
@@ -1,116 +1,116 @@
 { self, ... }:
 {
-	imports = [ ./bufferline.nix ];
+    imports = [ ./bufferline.nix ];
 
-  config = {
+    config = {
         colorschemes.gruvbox-material-nvim.enable = true;
-		globals = {
-			mapleader = " ";
-		};
+        globals = {
+            mapleader = " ";
+        };
 
-		opts = {
-			guicursor = "";
-			number = true;
-			relativenumber = true;
+        opts = {
+            guicursor = "";
+            number = true;
+            relativenumber = true;
 
-			shiftwidth = 4;
-			tabstop = 4;
-			softtabstop = 4;
-			expandtab = true;
+            shiftwidth = 4;
+            tabstop = 4;
+            softtabstop = 4;
+            expandtab = true;
 
-			smartindent = true;
-			wrap = false;
-			swapfile = false;
-			backup = false;
-			undodir = builtins.getEnv "HOME" + ".vim/undodir";
-			undofile = false;
+            smartindent = true;
+            wrap = false;
+            swapfile = false;
+            backup = false;
+            undodir = builtins.getEnv "HOME" + ".vim/undodir";
+            undofile = false;
 
-			hlsearch = true;
-			incsearch = true;
+            hlsearch = true;
+            incsearch = true;
 
-			termguicolors = true;
-			scrolloff = 98;
-			signcolumn = "yes";
+            termguicolors = true;
+            scrolloff = 98;
+            signcolumn = "yes";
 
-			updatetime = 50;
+            updatetime = 50;
 
-			colorcolumn = "80";
-		};
+            colorcolumn = "80";
+        };
 
-		keymaps = [
-		{
+        keymaps = [
+        {
             mode = "n";
-			action = "<cmd>Oil<CR>";
-			key = "<leader>pv";
-		}
-		{
+            action = "<cmd>Oil<CR>";
+            key = "<leader>pv";
+        }
+        {
             mode = "n";
-			action = "<cmd>Telescope find_files<CR>";
-			key = "<leader>pf";
-		}
-		{
+            action = "<cmd>Telescope find_files<CR>";
+            key = "<leader>pf";
+        }
+        {
             mode = "n";
-			action = "<cmd>Telescope git_files<CR>";
-			key = "<C-P>";
-		}
-		{
+            action = "<cmd>Telescope git_files<CR>";
+            key = "<C-P>";
+        }
+        {
             mode = "n";
-			action = "<cmd>Telescope grep_string({ search = vim.fn.input(\"Grep > \") })<CR>";
-			key = "<leader>ps";
-		}
-		];
+            action = "<cmd>Telescope grep_string({ search = vim.fn.input(\"Grep > \") })<CR>";
+            key = "<leader>ps";
+        }
+        ];
 
-		plugins = {
-			cmp = {
-				enable = true;
-				autoEnableSources = true;
-			};
-			lsp = {
-				enable = true;
-				servers = {
-					clangd = {
-						enable = true;
-						settings = {
-							cmd = [
-								"clangd"
-									"--background-index"
-							];
-							filetypes = [
-								"c"
-									"cpp"
-							];
-							root_markers = [
-								"compile_commands.json"
-									"compile_flags.txt"
-							];
-						};
-					};
-					ts_ls.enable = true;
-					lua_ls.enable = true;
-					rust_analyzer = {
-						enable = true;
-						installCargo = true;
-						installRustc = true;
-					};
-				};
-			};
-			lualine = {
-				enable = true;
-			};
-			luasnip = {
-				enable = true;
-			};
-			oil = {
-				enable = true;
-			};
-			telescope = {
-				enable = true;
-			};
-			treesitter = {
-				enable = true;
-			};
-		};
-		vimAlias = true;
-		viAlias = true;
-	};
+        plugins = {
+            cmp = {
+                enable = true;
+                autoEnableSources = true;
+            };
+            lsp = {
+                enable = true;
+                servers = {
+                    clangd = {
+                        enable = true;
+                        settings = {
+                            cmd = [
+                                "clangd"
+                                    "--background-index"
+                            ];
+                            filetypes = [
+                                "c"
+                                    "cpp"
+                            ];
+                            root_markers = [
+                                "compile_commands.json"
+                                    "compile_flags.txt"
+                            ];
+                        };
+                    };
+                    ts_ls.enable = true;
+                    lua_ls.enable = true;
+                    rust_analyzer = {
+                        enable = true;
+                        installCargo = true;
+                        installRustc = true;
+                    };
+                };
+            };
+            lualine = {
+                enable = true;
+            };
+            luasnip = {
+                enable = true;
+            };
+            oil = {
+                enable = true;
+            };
+            telescope = {
+                enable = true;
+            };
+            treesitter = {
+                enable = true;
+            };
+        };
+        vimAlias = true;
+        viAlias = true;
+    };
 }
